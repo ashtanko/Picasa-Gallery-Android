293ef0da2c40083d2621a4b1adc3f0b271d394e8
AdamLuisSean <adamluissean@gmail.com>
2017-08-08 00:21:21 +0300

    Add wrapper task

--- app/src/test/java/io/shtanko/picasagallery/ExampleUnitTest.kt
+++ app/src/test/java/io/shtanko/picasagallery/ExampleUnitTest.kt
@@ -1,14 +1,8 @@
 package io.shtanko.picasagallery
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
   @Test
   fun addition_isCorrect() {
